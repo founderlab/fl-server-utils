3cdb3315076eef3db8ab82eee6cf32caf0830fa6
gwilym <gwilym@gwilymh.com>
2015-12-17 15:03:49 +1100

    initial commit

---
+++ readme.md
@@ -0,0 +1,5 @@
+# Helper functions for the server side of FounderLab apps
+
+Changes: 
+
+- 0.1.0: Yoinked things from fl-base-webapp
---
+++ src/index.js
@@ -0,0 +1,56 @@
+import _ from 'lodash'
+import fs from 'fs'
+import path from 'path'
+
+const EXCLUDED_FILES = ['.DS_Store']
+
+export function removeDirectoryAndExtension(file, directory) {
+  const filename = file.replace(`${directory}/`, '')
+  return filename.replace(path.extname(filename), '')
+}
+
+export function filesInDir(directory) {
+  const results = []
+
+  function processDirectory(directory) {
+    if (!fs.existsSync(directory)) return
+    fs.readdirSync(directory).forEach(file => {
+      if (file in EXCLUDED_FILES) return
+
+      const pathed_file = path.join(directory, file)
+      const stat = fs.statSync(pathed_file)
+      // a directory, process
+      if (stat.isDirectory()) {
+        processDirectory(pathed_file)
+      }
+      else {
+         // a file, add to results
+        results.push(pathed_file)
+      }
+    })
+  }
+
+  processDirectory(directory)
+  return results
+}
+
+export function modulesInDir(directory) {
+  const results = {}
+  files(directory).forEach(file => {
+    try {
+      results[removeDirectoryAndExtension(file, directory)] = require(file)
+    }
+    catch (err) {
+      console.log(err)
+    }
+  })
+  return results
+}
+
+export function functionModulesInDir(directory) {
+  const results = modules(directory)
+  _.keys(results).forEach(file => {
+    if (!_.isFunction(results[file])) delete results[file]
+  })
+  return results
+}
