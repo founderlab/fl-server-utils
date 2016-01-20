ba1357d273f5f53dd8be95053df202ef6ed3fca2
gwilym <gwilym@gwilymh.com>
2016-01-21 09:02:38 +1100

    remove _rev from results of raw templates

--- src/backbone_rest.js
+++ src/backbone_rest.js
@@ -1,6 +1,16 @@
 import _ from 'lodash'
 import {JSONUtils} from 'backbone-orm'
 
+function stripRev(obj) {
+  const final_obj = {}
+  _.forEach(obj, (value, key) => {
+    if (key !== '_rev') {
+      final_obj[key] = _.isObject(value) ? stripRev(value) : value
+    }
+  })
+  return final_obj
+}
+
 export default function render(req, json, callback) {
   let template_name = req.query.$render || req.query.$template || this.default_template
   if (!template_name) return callback(null, json)
@@ -12,7 +22,12 @@ export default function render(req, json, callback) {
 
   const options = this.renderOptions ? this.renderOptions(req, template_name) : {}
 
-  if (template.$raw) return template(json, options, callback)
+  if (template.$raw) {
+    return template(json, options, (err, rendered_json) => {
+      if (err) return callback(err)
+      callback(null, stripRev(rendered_json))
+    })
+  }
 
   const models = _.isArray(json) ? _.map(json, (model_json) => new this.model_type(this.model_type.prototype.parse(model_json))) : new this.model_type(this.model_type.prototype.parse(json))
   JSONUtils.renderTemplate(models, template, options, callback)
